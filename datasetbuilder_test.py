9ee238453b93341cf5742b260589d2359dade2c7
haruhiko <morita.haruhiko.f1@s.mail.nagoya-u.ac.jp>
2022-06-17 19:31:39 +0900

    before creating dataset (first attempt)

--- datasetbuilder_test.py
+++ datasetbuilder_test.py
@@ -1,25 +1,25 @@
 import numpy as np
-import time
 import torch
 from makedata   import make_data
 from dataloader import Blur, CustomDataset
+import time
 torch.manual_seed(617)
 np.random.seed(617)
 
 device = (torch.device('cuda') if torch.cuda.is_available()
           else torch.device('cpu'))
 print(f"Training on device {device}.")
-
 t1 = time.time()
-inp     = make_data(1327104, (1920, 1536, 1536))
+inp     = make_data(13, (200, 200, 200))
+np.save('dataset_test/make_data_1e7.npy', inp)
 t2 = time.time()
+
 print(f'{t2 - t1} s', )
-np.save('dataset/make_data_1e7.npy', inp)
+time.sleep(60)
 print('make_data done!!')
-t3 = time.time()
 for _ in (1, 2, 4, 8, 12):
     dataset = CustomDataset(inp, _, 128, 128, 128, 141, 7, 7, bet_xy=6, bet_z=35)
     print(f'CustomDataset(x{str(_)}) done!!')
-    torch.save(dataset,f'dataset/dataset128_x{str(_)}.pt')
-t4 = time.time()
-print(f'{t4 - t3} s')
+    torch.save(dataset,f'dataset_test/testdataset128_x{str(_)}.pt')
+t3 = time.time()
+print(f'{t3 - t2} s')
