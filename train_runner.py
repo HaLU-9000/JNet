54339ff36e50c0b48b4f2cf50d8bdc7797fe1815
haruhiko <morita.haruhiko.f1@s.mail.nagoya-u.ac.jp>
2022-07-03 23:29:27 +0900

    before JNet_50_x8 train

--- train_runner.py
+++ train_runner.py
@@ -12,7 +12,7 @@ device = (torch.device('cuda') if torch.cuda.is_available()
 print(f"Training on device {device}.")
 PathDataset
 full_dataset = PathDataset(folderpath = 'datasetpath' ,
-                           imagename  = '_x2'         ,
+                           imagename  = '_x8'         ,    ###########
                            labelname  = '_label'      ,)
 train_size           = int(len(full_dataset) * 0.8)
 val_size             = len(full_dataset) - train_size
@@ -23,38 +23,35 @@ dataset, val_dataset = torch.utils.data.random_split(
 train_data  = DataLoader(dataset                                     ,
                          batch_size  = 2                             ,
                          shuffle     = True                          ,
-                         pin_memory  = False                         ,
+                         pin_memory  = True                         ,
                          num_workers = os.cpu_count()                ,)
 val_data    = DataLoader(val_dataset                                 ,
                          batch_size  = 2                             ,
                          shuffle     = False                         ,
-                         pin_memory  = False                         ,
+                         pin_memory  = True                         ,
                          num_workers = os.cpu_count()                ,)
 
-model_name              = 'JNet_43'
-hidden_channels_list    = [16, 32, 64, 128, 256]
-sr_hidden_channels_list = [64, 64]
-nblocks                 = 2
-activation              = nn.ReLU()
-dropout                 = 0.5
-torch.manual_seed(620)
-JNet = model.JNet(hidden_channels_list    = hidden_channels_list    ,
-                  nblocks                 = nblocks                 ,
-                  activation              = activation              ,
-                  dropout                 = dropout                 ,
-                  sr_hidden_channels_list = sr_hidden_channels_list ,
-                  sr_out_channels         = 8                       ,
-                  scale_factor            = 2                       ,
-                  scale                   = 2                       ,
-                  mu_z                    = 0.2                     ,
-                  sig_z                   = 0.2                     ,
-                  bet_xy                  = 6.                      ,
-                  bet_z                   = 35.                     ,)
+model_name              = 'JNet_50_x8' ################################
+hidden_channels_list = [16, 32, 64, 128, 256]
+scale_list           = [(2, 1, 1), (2, 1, 1), (2, 1, 1)]
+nblocks              = 2
+activation           = nn.ReLU()
+dropout              = 0.5
+torch.manual_seed(703)
+JNet = model.JNet(hidden_channels_list  = hidden_channels_list ,
+                  nblocks               = nblocks              ,
+                  activation            = activation           ,
+                  dropout               = dropout              ,
+                  scale_list            = scale_list           ,
+                  mu_z                  = 0.2                  ,
+                  sig_z                 = 0.2                  ,
+                  bet_xy                = 6.                   ,
+                  bet_z                 = 35.                  ,)
 JNet = JNet.to(device = device)
-optimizer            = optim.Adam(JNet.parameters(), lr = 1e-3)
+optimizer            = optim.Adam(JNet.parameters(), lr = 5e-4)
 loss_fn              = nn.BCELoss()
 train_loop(
-    n_epochs     = 2         ,
+    n_epochs     = 500       , ######################################
     optimizer    = optimizer ,
     model        = JNet      ,
     loss_fn      = loss_fn   ,
