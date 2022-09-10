389a18a4bcc58782e53bb60e8ac622e1aca45198
haruhiko <morita.haruhiko.f1@s.mail.nagoya-u.ac.jp>
2022-09-10 11:45:20 +0900

    107 inference done

--- inference.py
+++ inference.py
@@ -12,7 +12,7 @@ train_dataset = RandomCutDataset(folderpath  =  'randomdata'     ,  ###
                                  imagename   =  '_x1'            ,
                                  labelname   =  '_label'         ,
                                  size        =  (768, 768, 768)  ,
-                                 cropsize    =  (128, 128, 128)  ,
+                                 cropsize    =  (256,  64,  64)  ,
                                  I           =  200              ,
                                  low         =    0              ,
                                  high        =   16              ,
@@ -22,22 +22,22 @@ val_dataset   = RandomCutDataset(folderpath  =  'randomdata'     ,  ###
                                  imagename   =  '_x1'            ,
                                  labelname   =  '_label'         ,
                                  size        =  (768, 768, 768)  ,
-                                 cropsize    =  (128, 128, 128)  ,
-                                 I           =  200              ,
+                                 cropsize    =  (256,  64,  64)  ,
+                                 I           =   20              ,
                                  low         =   16              ,
                                  high        =   20              ,
                                  scale       =    1              ,
                                  train       = False             ,
                                  seed        = 907               ,
                                 )
-model_name           = 'JNet_77_x1'
+model_name           = 'JNet_107_x1'
 hidden_channels_list = [16, 32, 64, 128, 256]
 scale_list           = [(2, 1, 1)]
 nblocks              = 2
 s_nblocks            = 2
 activation           = nn.ReLU(inplace=True)
 dropout              = 0.5
-partial              = None
+partial              = (96, 192)
 JNet = model.JNet(hidden_channels_list  = hidden_channels_list ,
                   nblocks               = nblocks              ,
                   s_nblocks             = s_nblocks            ,
@@ -102,5 +102,5 @@ for n in range(0,5):
                 cmap='gray', vmin=0.0, vmax=1.0, aspect=1)
         ax6.imshow(label[0, :, i, :].to(device='cpu'),
                 cmap='gray', vmin=0.0, vmax=1.0, aspect=1)
-    plt.savefig(f'result/{model_name}_result{n}_softmax.png', format='png', dpi=250)
+    plt.savefig(f'result/{model_name}_result{n}.png', format='png', dpi=250)
     
