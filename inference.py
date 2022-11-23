37a4981ec1f4e205c0ef23cc68d2652afccb0d07
haruhiko <morita.haruhiko.f1@s.mail.nagoya-u.ac.jp>
2022-11-23 09:43:59 +0900

    132_x12 inference done

--- inference.py
+++ inference.py
@@ -9,43 +9,42 @@ device = (torch.device('cuda') if torch.cuda.is_available()
 print(f"Training on device {device}.")
 
 val_dataset   = RandomCutDataset(folderpath  =  'randomdata'     ,  ###
-                                 imagename   =  '_x1'            ,
+                                 imagename   =  '_x12'            ,
                                  labelname   =  '_label'         ,
                                  size        =  (768, 768, 768)  ,
-                                 cropsize    =  (256,  64,  64)  ,
+                                 cropsize    =  (240, 112, 112)  ,
                                  I           =   20              ,
                                  low         =   16              ,
                                  high        =   20              ,
-                                 scale       =    1              ,
+                                 scale       =   12              ,
                                  train       = False             ,
                                  seed        = 907               ,
                                 )
-model_name           = 'JNet_83_x1_partial'
+
+model_name           = 'JNet_132_x12'
 hidden_channels_list = [16, 32, 64, 128, 256]
-scale_list           = [(2, 1, 1)]
+scale_factor         = (12, 1, 1)
 nblocks              = 2
 s_nblocks            = 2
 activation           = nn.ReLU(inplace=True)
 dropout              = 0.5
-partial              = (64, 192)
+partial              = None #(56, 184)
 JNet = model.JNet(hidden_channels_list  = hidden_channels_list ,
                   nblocks               = nblocks              ,
-                  s_nblocks             = s_nblocks            ,
                   activation            = activation           ,
                   dropout               = dropout              ,
-                  scale_list            = scale_list           ,
+                  scale_factor          = scale_factor         ,
                   mu_z                  = 0.2                  ,
                   sig_z                 = 0.2                  ,
                   bet_xy                = 6.                   ,
                   bet_z                 = 35.                  ,
-                  superres              = False                ,
-                  use_gumbelsoftmax     = True                 ,
+                  superres              = True                 ,
                   )
 JNet = JNet.to(device = device)
 JNet.set_tau(1)
-j = 60
+j = 5
 i = 30
-scale = 1
+scale = 12
 
 JNet.load_state_dict(torch.load(f'model/{model_name}.pt'), strict=False)
 JNet.eval()
