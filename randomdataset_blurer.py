79844e30b32a86351fc0484ea0f5485341c987cb
haruhiko <morita.haruhiko.f1@s.mail.nagoya-u.ac.jp>
2022-12-05 10:28:04 +0900

    making beadslikedata 3

--- randomdataset_blurer.py
+++ randomdataset_blurer.py
@@ -11,20 +11,20 @@ blurring
 device = (torch.device('cuda') if torch.cuda.is_available()
           else torch.device('cpu'))
 print(f"Training on device {device}.")
-
-for scale in (1, 10):
-    model = Blur(scale  = scale ,
-                z       = 141   ,
-                x       = 7     ,
-                y       = 7     ,
-                mu_z    = 0.2   ,
-                sig_z   = 0.2   , 
-                bet_xy  = 3.    ,
-                bet_z   = 17.5  ,
-                sig_eps = 0.02  ,)
+scale = 10
+if scale == 10:
+    model = Blur(scale   = scale ,
+                 z       = 71    ,
+                 x       = 5     ,
+                 y       = 5     ,
+                 mu_z    = -2.114,
+                 sig_z   = 0.675 , 
+                 bet_xy  = 3     ,
+                 bet_z   = 17.5  ,
+                 sig_eps = 0.01  ,)
     model.eval
     folderpath    = 'beadslikedataset2'
-    outfolderpath = 'beadslikedata2'
+    outfolderpath = 'beadslikedata3'
     labelname     = '_label'
     outlabelname  = '_label'
     save_dataset(model, folderpath, outfolderpath, labelname, outlabelname, scale, 0)
