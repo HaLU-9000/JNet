e9ade3a134b3d867c4222b0a6d368508156d955d
haruhiko <morita.haruhiko.f1@s.mail.nagoya-u.ac.jp>
2023-03-08 22:00:39 +0900

    opticsbased start

--- inference.py
+++ inference.py
@@ -54,7 +54,7 @@ JNet = model.JNet(hidden_channels_list  = hidden_channels_list ,
                   )
 JNet = JNet.to(device = device)
 JNet.set_tau(1)
-j = 60
+j = 120
 i = 60
 j_s = j // scale
 
@@ -70,30 +70,30 @@ for n in range(0,1):
     reconst = reconst.squeeze(0).detach().cpu().numpy()
 
     fig = plt.figure(figsize=(25, 15))
-    ax1 = fig.add_subplot(241)
-    ax2 = fig.add_subplot(242)
-    ax3 = fig.add_subplot(243)
-    ax4 = fig.add_subplot(244)
-    ax5 = fig.add_subplot(245)
-    ax6 = fig.add_subplot(246)
-    ax7 = fig.add_subplot(247)
-    ax8 = fig.add_subplot(248)
+    ax1 = fig.add_subplot(231)
+    ax2 = fig.add_subplot(232)
+    ax3 = fig.add_subplot(233)
+    ax4 = fig.add_subplot(234)
+    ax5 = fig.add_subplot(235)
+    ax6 = fig.add_subplot(236)
+    #ax7 = fig.add_subplot(247)
+    #ax8 = fig.add_subplot(248)
     ax1.set_axis_off()
     ax2.set_axis_off()
     ax3.set_axis_off()
     ax4.set_axis_off()
     ax5.set_axis_off()
     ax6.set_axis_off()
-    ax7.set_axis_off()
-    ax8.set_axis_off()
-    ax1.set_title('plain\nreconstruct image')
-    ax2.set_title('plain\noriginal image')
-    ax3.set_title('plain\nsegmentation result')
-    ax4.set_title('plain\nlabel')
-    ax5.set_title('depth\nreconstruct')
-    ax6.set_title('depth\noriginal image')
-    ax7.set_title('depth\nsegmentation result')
-    ax8.set_title('depth\nlabel')
+    #ax7.set_axis_off()
+    #ax8.set_axis_off()
+    #ax1.set_title('plain\nreconstruct image')
+    #ax2.set_title('plain\noriginal image')
+    #ax3.set_title('plain\nsegmentation result')
+    #ax4.set_title('plain\nlabel')
+    #ax5.set_title('depth\nreconstruct')
+    #ax6.set_title('depth\noriginal image')
+    #ax7.set_title('depth\nsegmentation result')
+    #ax8.set_title('depth\nlabel')
     plt.subplots_adjust(hspace=-0.0)
     if partial is not None:
         ax1.imshow(reconst[0, partial[0]+j_s, :, :],
@@ -113,21 +113,21 @@ for n in range(0,1):
         ax8.imshow(label[0, partial[0]:partial[1], i, :].to(device='cpu'),
                 cmap='gray', vmin=0.0, vmax=1.0, aspect=1)
     else:
-        ax1.imshow(reconst[0, j_s, :, :],
+        #ax1.imshow(reconst[0, j_s, :, :],
+        #        cmap='gray', vmin=0.0, vmax=1.0, aspect=1)
+        ax1.imshow(image[0, j_s, :, :].to(device='cpu'),
+                cmap='gray', vmin=0.0, aspect=1)
+        ax2.imshow(output[0, 0, j, :, :],
                 cmap='gray', vmin=0.0, vmax=1.0, aspect=1)
-        ax2.imshow(image[0, j_s, :, :].to(device='cpu'),
+        ax3.imshow(label[0, j, :, :].to(device='cpu'),
                 cmap='gray', vmin=0.0, vmax=1.0, aspect=1)
-        ax3.imshow(output[0, 0, j, :, :],
+        #ax5.imshow(reconst[0, :, i, :],
+        #        cmap='gray', vmin=0.0, vmax=1.0, aspect=scale)
+        ax4.imshow(image[0, :, i, :].to(device='cpu'),
+                cmap='gray', vmin=0.0, aspect=scale)
+        ax5.imshow(output[0, 0, :, i, :],
                 cmap='gray', vmin=0.0, vmax=1.0, aspect=1)
-        ax4.imshow(label[0, j, :, :].to(device='cpu'),
-                cmap='gray', vmin=0.0, vmax=1.0, aspect=1)
-        ax5.imshow(reconst[0, :, i, :],
-                cmap='gray', vmin=0.0, vmax=1.0, aspect=scale)
-        ax6.imshow(image[0, :, i, :].to(device='cpu'),
-                cmap='gray', vmin=0.0, vmax=1.0, aspect=scale)
-        ax7.imshow(output[0, 0, :, i, :],
-                cmap='gray', vmin=0.0, vmax=1.0, aspect=1)
-        ax8.imshow(label[0, :, i, :].to(device='cpu'),
+        ax6.imshow(label[0, :, i, :].to(device='cpu'),
                 cmap='gray', vmin=0.0, vmax=1.0, aspect=1)
     plt.savefig(f'result_psj/{model_name}_sim_result{n}.png', format='png', dpi=250)
     
