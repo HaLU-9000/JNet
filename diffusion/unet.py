9dbab5711fe63317a911d0289257f7b83fe39072
haruhiko <morita.haruhiko.f1@s.mail.nagoya-u.ac.jp>
2023-04-04 14:50:31 +0900

    before copying

--- diffusion/unet.py
+++ diffusion/unet.py
@@ -1,6 +1,6 @@
 # unet for diffusion
 # num block variable
-# add condition vec
+# added condition vec (c)
 
 import torch
 import torch.nn as nn
@@ -9,6 +9,8 @@ import torch.nn.functional as F
 # resblock (cond)
 class ResBlock(nn.Module):
     def __init__(self,):
+        
+        # time embedding
         pass
     def forward(self, x, t, c):
         pass
@@ -21,21 +23,28 @@ class Attention(nn.Module):
         pass
 
 # downblock
-class DownBlock(nn.Module):
+class DownSample(nn.Module):
     def __init__(self,):
         pass
     def forward(self, x, t, c):
         pass
 
 # upblock
-class UpBlock(nn.Module):
+class UpSample(nn.Module):
     def __init__(self,):
         pass
     def forward(self, x, t, c):
         pass
-    
+
 # middleblock
 class MiddleBlock(nn.Module):
+    def __init__(self,):
+        pass
+    def forward(self, x, t, c):
+        pass
+
+# conbine
+class Unet(nn.Module):
     def __init__(self,):
         pass
     def forward(self, x, t, c):
