ed4cf0974647fdbe91fc66051e1ad908995a2ba5
haruhiko <morita.haruhiko.f1@s.mail.nagoya-u.ac.jp>
2023-04-04 11:50:06 +0900

    unet define class

--- diffusion/unet.py
+++ diffusion/unet.py
@@ -1,1 +1,42 @@
 # unet for diffusion
+# num block variable
+# add condition vec
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# resblock (cond)
+class ResBlock(nn.Module):
+    def __init__(self,):
+        pass
+    def forward(self, x, t, c):
+        pass
+
+# attentionblock
+class Attention(nn.Module):
+    def __init__(self,):
+        pass
+    def forward(self, x, t, c):
+        pass
+
+# downblock
+class DownBlock(nn.Module):
+    def __init__(self,):
+        pass
+    def forward(self, x, t, c):
+        pass
+
+# upblock
+class UpBlock(nn.Module):
+    def __init__(self,):
+        pass
+    def forward(self, x, t, c):
+        pass
+    
+# middleblock
+class MiddleBlock(nn.Module):
+    def __init__(self,):
+        pass
+    def forward(self, x, t, c):
+        pass
