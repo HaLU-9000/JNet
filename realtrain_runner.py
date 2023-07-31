abea4a2dd9609b593854c6ecce789a73a1b62106
haruhiko <morita.haruhiko.f1@s.mail.nagoya-u.ac.jp>
2023-07-31 16:38:49 +0900

    during 259 training

--- realtrain_runner.py
+++ realtrain_runner.py
@@ -1,52 +1,56 @@
 import os
+import numpy as np
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 import torch.optim as optim
 import model as model
-from dataset import RandomCutDataset
+from dataset import RealDensityDataset
 from   train_loop import train_loop
 
 device = (torch.device('cuda') if torch.cuda.is_available()
           else torch.device('cpu'))
 print(f"Training on device {device}.")
 
-scale    = 6
+scale    = 10
 surround = False
 surround_size = [32, 4, 4]
-train_score   = None 
-val_score     = None 
+train_score   = torch.load('./beadsscore4/002_score.pt') #torch.load('./sparsebeadslikescore/_x10_score.pt') #torch.load('./beadsscore/001_score.pt')
+val_score     = torch.load('./beadsscore4/002_score.pt') #None #torch.load('./sparsebeadslikescore/_x10_score.pt') #
 
-train_dataset = RandomCutDataset(folderpath  =  '_var_num_beadsdata2_20' ,  ###
-                                 imagename   =  f'_x{scale}'          , 
-                                 labelname   =  '_label'              ,
-                                 size        =  (1200, 500, 500)      ,
-                                 cropsize    =  ( 240, 112, 112)      , 
-                                 I             =   2                  ,
-                                 low           =   0                  ,
-                                 high          =  16                  ,
-                                 scale         =  scale               ,  ## scale
-                                 mask          =  True                ,
-                                 mask_size     =  [ 1, 10, 10]        ,
-                                 mask_num      =  30                  ,  #( 1% of image)
-                                 surround      =  surround            ,
-                                 surround_size =  surround_size       ,
-                                 )
-val_dataset   = RandomCutDataset(folderpath  =  '_var_num_beadsdata2_20'   ,  ###
-                                 imagename   =  f'_x{scale}'            ,     ## scale
-                                 labelname   =  '_label'                ,
-                                 size        =  (1200, 500, 500)        ,
-                                 cropsize    =  ( 240, 112, 112)        ,
-                                 I             =   2                    ,
-                                 low           =  16                    ,
-                                 high          =  20                    ,
-                                 scale         =  scale                 ,   ## scale
-                                 train         =  False                 ,
-                                 mask          =  False                 ,
-                                 surround      =  surround              ,
-                                 surround_size =  surround_size         ,
-                                 seed          =  907                   ,
-                                )       
+train_dataset = RealDensityDataset(folderpath      =  'beadsdata4' ,
+                                   scorefolderpath =  'beadsscore4',
+                                   imagename       =  '002'            ,
+                                   size            =  ( 650, 512, 512) , # size after segmentation
+                                   cropsize        =  ( 240, 112, 112) , # size after segmentation
+                                   I               = 200               ,
+                                   low             =   0               ,
+                                   high            =   1               ,
+                                   scale           =  scale            ,   ## scale
+                                   train           =  True             ,
+                                   mask            =  True             ,
+                                   mask_num        =  10               ,
+                                   mask_size       =  [1, 10, 10]      ,
+                                   surround        =  surround         ,
+                                   surround_size   =  surround_size    ,
+                                   score           =  train_score      ,
+                                  )
+val_dataset   = RealDensityDataset(folderpath      =  'beadsdata4' ,
+                                   scorefolderpath =  'beadsscore4',
+                                   imagename       =  '002'            ,
+                                   size            =  ( 650, 512, 512) , # size after segmentation
+                                   cropsize        =  ( 240, 112, 112) ,
+                                   I               =  10               ,
+                                   low             =   0               ,
+                                   high            =   1               ,
+                                   scale           =  scale            ,
+                                   train           =  False            ,
+                                   mask            =  False            ,
+                                   surround        =  False            ,
+                                   surround_size   =  [64, 8, 8]       ,
+                                   seed            =  1204             ,
+                                   score           =  val_score        ,
+                                  )
 
 train_data  = DataLoader(train_dataset                 ,
                          batch_size  = 1               ,
@@ -61,7 +65,7 @@ val_data    = DataLoader(val_dataset                   ,
                          num_workers = os.cpu_count()  ,
                          )
 
-model_name           = 'JNet_250_ez0updatecheck'
+model_name           = 'JNet_259_30_finetuning'
 hidden_channels_list = [16, 32, 64, 128, 256]
 nblocks              = 2
 s_nblocks            = 2
@@ -69,23 +73,21 @@ activation           = nn.ReLU(inplace=True)
 dropout              = 0.5
 partial              = None #(56, 184)
 superres = True if scale > 1 else False
-params               = {"mu_z"   : 0.2    ,
-                        "sig_z"  : 0.2    ,
-                        "bet_z"  : 30.    ,
-                        "bet_xy" : 1.0    ,
-                        "alpha"  : 1.     ,
-                        "sig_eps": 0.01   ,
-                        "scale"  : 6
-                        }                   
+params               = {"mu_z"       : 0.2               ,
+                        "sig_z"      : 0.2               ,
+                        "log_bet_z"  : np.log(30.).item(),
+                        "log_bet_xy" : np.log(1.).item() ,
+                        "log_alpha"  : np.log(1.).item() ,
+                        "sig_eps": 0.01                  ,
+                        "scale"  : 6                     ,
+                        }
 reconstruct = True
-coeff_bet_z = 10
 param_estimation_list = [False, False, False, False, True]
 JNet = model.JNet(hidden_channels_list  = hidden_channels_list ,
                   nblocks               = nblocks              ,
                   activation            = activation           ,
                   dropout               = dropout              ,
                   params                = params               ,
-                  coeff_bet_z           = coeff_bet_z          ,
                   param_estimation_list = param_estimation_list,
                   superres              = superres             ,
                   reconstruct           = reconstruct          ,
@@ -95,17 +97,17 @@ JNet = model.JNet(hidden_channels_list  = hidden_channels_list ,
 JNet = JNet.to(device = device)
 JNet.load_state_dict(torch.load('model/JNet_241_x6_largeblur-pretrain.pt'),
                      strict=False)
-init_ez0 = torch.exp(torch.tensor(params["mu_z"]) + 0.5 \
-                     * torch.tensor(params["sig_z"]) ** 2)
-JNet.image.emission.ez0.data =  init_ez0
-JNet.image.blur.bet_xy.data  =  torch.tensor(params["bet_xy"]).to(device)
-JNet.image.blur.bet_z.data   =  torch.tensor(params["bet_z"]/coeff_bet_z).to(device)
-JNet.image.blur.alpha.data   =  torch.tensor(params["alpha"]).to(device)
+init_log_ez0 = (torch.tensor(params["mu_z"]) + 0.5 \
+                * torch.tensor(params["sig_z"]) ** 2).to(device)
+JNet.image.emission.log_ez0.data = init_log_ez0
+JNet.image.blur.log_bet_xy.data  = torch.tensor(params["log_bet_xy"]).to(device)
+JNet.image.blur.log_bet_z.data   = torch.tensor(params["log_bet_z"]).to(device)
+JNet.image.blur.log_alpha.data   = torch.tensor(params["log_alpha"]).to(device)
 print([i for i in JNet.parameters()][-4:])
 
 params = JNet.parameters()
 
-optimizer            = optim.Adam(params, lr = 1e-2)
+optimizer            = optim.Adam(params, lr = 1e-3)
 scheduler            = None #= optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=5, verbose=True)
 loss_fn              = nn.MSELoss()
 midloss_fn           = nn.BCELoss()
@@ -134,5 +136,5 @@ train_loop(
     loss_weight      = 1           ,
     qloss_weight     = 1           ,
     paramloss_weight = 0           ,
-    verbose          = True        ,
+    verbose          = False       ,
     )
