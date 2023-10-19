88a98b90222c3a67486fd7b8b36c141e918ec5c1
haruhiko <morita.haruhiko.f1@s.mail.nagoya-u.ac.jp>
2023-10-19 10:50:33 +0900

    364 done

--- experiments/bash/common.sh
+++ experiments/bash/common.sh
@@ -1,3 +1,3 @@
-python3 train_runner.py      JNet_362
-python3 realtrain_runner.py  JNet_362
-python3 reporter.py          JNet_362
+python3 train_runner.py      JNet_364
+python3 realtrain_runner.py  JNet_364
+python3 reporter.py          JNet_364
