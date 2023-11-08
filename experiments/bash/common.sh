f833e912ffd513569a50ccbcfbbd04041e2c90a9
haruhiko <morita.haruhiko.f1@s.mail.nagoya-u.ac.jp>
2023-11-08 10:38:05 +0900

    405 bash

--- experiments/bash/common.sh
+++ experiments/bash/common.sh
@@ -1,3 +1,3 @@
-#python3 train_runner.py      JNet_391
-python3 realtrain_runner.py  JNet_404
-python3 reporter.py          JNet_404
+python3 train_runner.py      JNet_405
+python3 realtrain_runner.py  JNet_405
+python3 reporter.py          JNet_405
