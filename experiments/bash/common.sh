8366d6a4dbf40a82b9d6b578a75c8d326cf88bff
haruhiko <morita.haruhiko.f1@s.mail.nagoya-u.ac.jp>
2023-11-11 19:47:48 +0900

    452, 453 start

--- experiments/bash/common.sh
+++ experiments/bash/common.sh
@@ -1,17 +1,6 @@
-python3 train_runner.py      JNet_443
-python3 realtrain_runner.py  JNet_443
-python3 reporter.py          JNet_443
-python3 realtrain_runner.py  JNet_444
-python3 reporter.py          JNet_444
+python3 train_runner.py      JNet_452
+python3 realtrain_runner.py  JNet_452
+python3 reporter.py          JNet_452
 
-python3 train_runner.py      JNet_446
-python3 realtrain_runner.py  JNet_446
-python3 reporter.py          JNet_446
-python3 realtrain_runner.py  JNet_447
-python3 reporter.py          JNet_447
-
-python3 train_runner.py      JNet_449
-python3 realtrain_runner.py  JNet_449
-python3 reporter.py          JNet_449
-python3 realtrain_runner.py  JNet_450
-python3 reporter.py          JNet_450
+python3 realtrain_runner.py  JNet_453
+python3 reporter.py          JNet_453
