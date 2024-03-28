42ecf355042703c1f2d3b4cd5deff43ba8776c15
HaLU-9000 <morita.haruhiko.f1@s.mail.nagoya-u.ac.jp>
2024-03-28 11:00:51 +0900

    518 train started

--- experiments/bash/common.sh
+++ experiments/bash/common.sh
@@ -1,2 +1,2 @@
-python3 train_runner.py JNet_517 --train_align
-python3 finetuning.py   JNet_517 --train_with_align
+python3 train_runner.py JNet_518 --train_align
+python3 finetuning.py   JNet_518 --train_with_align
