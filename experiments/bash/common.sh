ddd79ebcceb6c31a738f7aec08e7ad0b10ee8f8c
HaLU-9000 <morita.haruhiko.f1@s.mail.nagoya-u.ac.jp>
2024-03-30 23:55:35 +0900

    train started on bengio

--- experiments/bash/common.sh
+++ experiments/bash/common.sh
@@ -1,7 +1,7 @@
 # for KUBRICK
-python3 train_runner.py JNet_529
-python3 finetuning.py   JNet_529
-python3 finetuning.py   JNet_530
+#python3 train_runner.py JNet_529
+#python3 finetuning.py   JNet_529
+#python3 finetuning.py   JNet_530
 #python3 finetuning_with_simulation.py JNet_531
 # for Bengio screen 1, GPU=5
 #python3 train_runner.py JNet_533
@@ -13,11 +13,11 @@ python3 finetuning.py   JNet_530
 #python3 finetuning.py   JNet_535 --train_with_align
 ##python3 finetuning_with_simulation.py JNet_536 --train_with_align
 ## for Bengio screen 2, GPU=6
-#python3 train_runner.py JNet_538
-#python3 train_runner.py JNet_538 --train_align
-#python3 finetuning.py   JNet_538 --train_with_align
-#python3 train_runner.py JNet_539 --train_align
-#python3 finetuning.py   JNet_539 --train_with_align
-#python3 train_runner.py JNet_540 --train_align
-#python3 finetuning.py   JNet_540 --train_with_align
+python3 train_runner.py JNet_538
+python3 train_runner.py JNet_538 --train_align
+python3 finetuning.py   JNet_538 --train_with_align
+python3 train_runner.py JNet_539 --train_align
+python3 finetuning.py   JNet_539 --train_with_align
+python3 train_runner.py JNet_540 --train_align
+python3 finetuning.py   JNet_540 --train_with_align
 ##python3 finetuning_with_simulation.py JNet_541 --train_with_align
